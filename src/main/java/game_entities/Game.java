0f3587e2322d9f1153f09ae596b5dd1cf3eab2c8
eric <ericshi97@gmail.com>
2022-12-06 18:24:08 -0500

    added 4 methods: dealCards(), findWinner(), nextPlayer(), nextRound(). Note: findWinner() has not yet been linked with the pool class yet.

--- src/main/java/game_entities/Game.java
+++ src/main/java/game_entities/Game.java
@@ -3,14 +3,24 @@ package game_entities;
 import game_entities.Card;
 import game_entities.Deck;
 
+import java.util.*;
+
 public class Game {
     private final Player[] players;
     private final Pool pool;
     private final Deck deck;
     private final Card[] cards;
 
-    private int currentWager = 0;
-    private int currentPlayer = 0;
+    private int currentWager;
+    private int currentPlayer;
+
+    private int firstPlayer;
+
+    private int[] playerBets;
+
+    private int lastBet; //index of the last person who bet
+
+    private Boolean[] isActive;
 
     public Game(Player[] players){
         this.pool = new Pool(players);
@@ -30,6 +40,291 @@ public class Game {
         this.cards[3] = card4;
         this.cards[4] = card5;
     }
+
+    /**
+     * This method will add cards to the table
+     */
+    public void dealCards() {
+        if (this.cards.length == 0) {
+            cards[0] = this.deck.getCard();
+            cards[1] = this.deck.getCard();
+            cards[2] = this.deck.getCard();
+        } else if (this.cards.length == 3) {
+            cards[3] = this.deck.getCard();
+        } else if (this.cards.length == 4) {
+            cards[4] = this.deck.getCard();
+        }
+    }
+
+    public void nextPlayer() {
+        if (currentPlayer <= players.length) {
+            currentPlayer++;
+        } else {
+            currentPlayer = 0;
+        }
+        //you can remove this bit if you want
+        if (currentPlayer == lastBet) {
+            currentPlayer = -1;
+        }
+
+    }
+
+    public void nextRound() {
+        if (firstPlayer <= players.length) {
+            firstPlayer++;
+        } else {
+            firstPlayer = 0;
+        }
+
+        this.dealCards();
+
+        currentWager = 0;
+        currentPlayer = firstPlayer;
+    }
+
+    /**
+     * This method will take in a list of players and the card flop and will output the list of winning players
+     * @param players
+     * @param flop
+     * @return an array of players with their respective ranking
+     */
+    public int[] findWinner(Player[] players, String[] flop) {
+        int[] scores = new int[players.length];
+
+
+        String[] hand = new String[2];
+        String tempCard = "";
+        for (int i = 0; i < players.length; i++) {
+            for (int j =0; j < players[i].getCards().length; j++) {
+                tempCard = players[i].getCards()[j].toString();
+                hand[j] = tempCard.substring(1) + tempCard.charAt(0);
+            }
+            scores[i] = calculateHand(hand, flop);
+        }
+
+        int[] rankings = new int[players.length];
+        for (int x =0; x < rankings.length; x++) {
+            rankings[x]++;
+        }
+        for (int i = 0; i < scores.length; i++) {
+            for (int j =0; j < scores.length; j++) {
+                if (scores[j] > scores[i]) {
+                    rankings[i]++;
+                }
+            }
+        }
+        //this can pass into the pool class later
+        return rankings;
+    }
+
+    /**
+     * This is a wrapper method
+     * @param hand1
+     * @param hand2
+     * @return 0 for a tie, 1 for hand1, 2 for hand2
+     */
+    public int calcTwoWin(String[] hand1, String[] hand2) {
+
+        if (calculateHand(hand1) > calculateHand(hand2)) {
+            return 1;
+        } else if (calculateHand(hand1) < calculateHand(hand2)) {
+            return 2;
+        }
+        return 0;
+    }
+
+    /**
+     * This function will return the score of a hand
+     * @param fused array of cards
+     * @return the score of the hand as an int from 1 to 130 depending on the kicker of the hand and its highest card
+     */
+    public int calculateHand(String[] fused) {
+        //hand flag
+        boolean isStraight = false;
+        boolean isFlush = false;
+        boolean is3Same = false;
+        boolean is2Same = false;
+
+        for (int i = 0; i < fused.length - 1; i++) {
+            for (int j = 0; j < fused.length - i - 1; j++) {
+                if (fused[j].charAt(1) > fused[j + 1].charAt(1)) {
+                    // swap arr[j+1] and arr[j]
+                    String temp = fused[j];
+                    fused[j] = fused[j + 1];
+                    fused[j + 1] = temp;
+                }
+            }
+        }
+
+
+        Integer[] rankArr = new Integer[7];
+        char[] suitArr = new char[7];
+        for (int i = 0; i < fused.length; i++) {
+            if (fused[i].charAt(1) == 'T') {
+                rankArr[i] = 10;
+            } else if (fused[i].charAt(1) == 'J') {
+                rankArr[i] = 11;
+            } else if (fused[i].charAt(1) == 'Q') {
+                rankArr[i] = 12;
+            } else if (fused[i].charAt(1) == 'K') {
+                rankArr[i] = 13;
+            } else {
+                rankArr[i] = fused[i].charAt(1) - '0';
+            }
+            suitArr[i] = fused[i].charAt(0);
+        }
+
+
+
+        //check isStraight
+        isStraight = checkStraight(rankArr)[0] == 1;
+        int straightMax = checkStraight(rankArr)[1];
+
+        //check isFlush
+        ArrayList<Integer> hCards = new ArrayList<>();
+        ArrayList<Integer> dCards = new ArrayList<>();
+        ArrayList<Integer> cCards = new ArrayList<>();
+        ArrayList<Integer> sCards = new ArrayList<>();
+        Integer[] flushC = new Integer[0];
+
+        for (int i = 0; i < suitArr.length; i++) {
+            if (suitArr[i] == 'H')
+                hCards.add(rankArr[i]);
+            else if (suitArr[i] == 'D')
+                dCards.add(rankArr[i]);
+            else if (suitArr[i] == 'C')
+                cCards.add(rankArr[i]);
+            else
+                sCards.add(rankArr[i]);
+        }
+        if (hCards.size() >= 5) {
+            isFlush = true;
+            flushC = hCards.toArray(new Integer[0]);
+        } else if (dCards.size() >= 5) {
+            isFlush = true;
+            flushC = dCards.toArray(new Integer[0]);
+        } else if (cCards.size() >= 5) {
+            isFlush = true;
+            flushC = dCards.toArray(new Integer[0]);
+        } else if (sCards.size() >= 5) {
+            isFlush = true;
+            flushC = sCards.toArray(new Integer[0]);
+        }
+
+        if (isFlush) {
+            if (checkStraight(flushC)[0] == 1) {
+                return 117 + (flushC[1] == 1 ? flushC[flushC.length - 1] - 1 : 13);
+            }
+        }
+
+        //check pair
+        HashMap<Integer, Integer> nums = new HashMap<Integer, Integer>();
+        for (int i = 0; i < rankArr.length; i++) {
+            if (nums.containsKey(rankArr[i])) {
+                nums.put(rankArr[i], nums.get(rankArr[i]) + 1);
+            } else {
+                nums.put(rankArr[i], 1);
+            }
+        }
+
+        ArrayList<Integer> pairs = new ArrayList<>();
+        ArrayList<Integer> trios = new ArrayList<>();
+
+        for (Integer i : nums.keySet()) {
+            if (nums.get(i) == 2) {
+                is2Same = true;
+                pairs.add(i);
+            } else if (nums.get(i) == 3) {
+                is3Same = true;
+                trios.add(i);
+            } else if (nums.get(i) == 4) {
+                return 91 + (i != 1 ? i - 1 : 13);
+            }
+        }
+
+        Collections.sort(pairs);
+        Collections.sort(trios);
+
+        if (is3Same && is2Same || trios.size() > 1) {
+            return 78 + (trios.get(0) != 1 ? trios.get(trios.size() - 1) - 1 : 13); //this is broken, need to change this
+        } else if (isFlush) {
+            return 65 + (flushC[0] != 1 ? flushC[flushC.length - 1] - 1 : 13);
+        } else if (isStraight) {
+            return 52 + (straightMax != 1 ? straightMax - 1 : 13);
+        } else if (is3Same) {
+            return 39 + (trios.get(0) != 1 ? trios.get(trios.size() - 1) - 1 : 13);
+        } else if (pairs.size() >= 2) {
+            return 26 + (pairs.get(0) != 1 ? pairs.get(pairs.size() - 1) - 1 : 13); //this is also broken
+        } else if (is2Same) {
+            return 13 + (pairs.get(0) != 1 ? pairs.get(pairs.size() - 1) - 1 : 13);
+        }
+
+        return rankArr[rankArr.length - 1];
+    }
+
+    /**
+     * Helper method for checking if its a straight
+     * @param rankArr
+     * @return an int array where the first index is if its a straight and the second is the highest value in the straight
+     */
+    private int[] checkStraight(Integer[] rankArr) {
+        int counter = 1;
+
+        Integer[] unique = new HashSet<Integer>(Arrays.asList(rankArr)).toArray(new Integer[0]);
+
+        Arrays.sort(unique);
+
+        if (unique[0] == 1) {
+            boolean aceStraight = true;
+            int temp = 14 - unique.length;
+            for (int i = unique.length - 4; i < unique.length; i++) {
+
+                if (unique[i] != i + temp) {
+                    aceStraight = false;
+                }
+            }
+
+            if (aceStraight) {
+                return new int[]{1, 1};
+            }
+        }
+        for (int i = 1; i < unique.length; i++) {
+            if (unique[i - 1] == unique[i] - 1) {
+                counter++;
+            } else {
+                if (counter >= 5) {
+                    return new int[]{1, unique[i - 2]};
+                } else {
+                    counter = 1;
+                }
+            }
+
+        }
+        if (counter >= 5) {
+            return new int[]{1, unique[rankArr.length - 1]};
+        }
+        return new int[]{0, 0};
+    }
+
+    /**
+     * Wrapper method for separate hand array and river array
+     * @param hand
+     * @param river
+     * @return the score of the hand
+     */
+    public int calculateHand(String[] hand, String[] river) {
+        String[] fused = new String[7];
+        for (int i = 0; i < 7; i++) {
+            if (i < 5) {
+                fused[i] = river[i];
+            } else {
+                fused[i] = hand[i - 5];
+            }
+        }
+
+        return calculateHand(fused);
+    }
+
 
     public int getCurrentWager() {
         return this.currentWager;
@@ -192,10 +487,11 @@ public class Game {
         return result;
     }
 
+    // return the winner's index iff there is only 1 non-negative integer in <l>
+    // return -1 if there are more than 1 non-zero integers in <l>
+    // this method only checks the situation where everyone else folds
     private int checkWinner(int[] l){  // tested!
-        // return the winner's index iff there is only 1 non-negative integer in <l>
-        // return -1 if there are more than 1 non-zero integers in <l>
-        // this method only checks the situation where everyone else folds
+
         int count = 0;
         int index = -1;
         for (int i = 0; i < l.length; i++){
@@ -210,7 +506,6 @@ public class Game {
             return -1;
         }
     }
-
 
     private void processDecision(Player[] players, int index, String d, Pool pl, int currentCall, int[] callLst){
 
@@ -227,7 +522,6 @@ public class Game {
             callLst[index] = 0;
         }
     }
-
 
     public Player[] getPlayers() {
         return players;
@@ -263,4 +557,6 @@ public class Game {
     public int getCurrentPlayerAsInt() {
         return currentPlayer;
     }
+
+
 }
