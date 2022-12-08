d16e039c4ce51ddef450afb4a5986eae3bd9dce9
Andy-Guo-13 <andy.guo21@ycdsbk12.ca>
2022-12-08 02:23:34 -0500

    Implement teset case for check

--- src/test/java/game_use_case/CheckInteractorTest.java
+++ src/test/java/game_use_case/CheckInteractorTest.java
@@ -8,10 +8,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckInteractorTest {
     /**
+     * Test case for a 2 player game when player 1 checks
+     */
+    @Test
+    void player1Check() {
+        CheckPresenter presenter = new CheckPresenter() {
+            @Override
+            public ResponseModel prepareSuccessView(ResponseModel outputData) {
+                // Test for correct balance
+                assertEquals(100, outputData.getPlayerBalance()[0]);
+                assertEquals(100, outputData.getPlayerBalance()[1]);
+                assertEquals(0, outputData.getPlayerBets()[0]);
+                assertEquals(0, outputData.getPlayerBets()[1]);
+                // Test for correct user turn and last bet amount
+                assertEquals(1, outputData.getCurrentPlayer());
+                assertEquals(0, outputData.getCurrentBet());
+                // Test if table cards are correct
+                assertEquals("DA", outputData.getTableCard()[0]);
+                assertEquals("DK", outputData.getTableCard()[1]);
+                assertEquals("DQ", outputData.getTableCard()[2]);
+                assertNull(outputData.getTableCard()[3]);
+                assertNull(outputData.getTableCard()[4]);
+                return null;
+            }
+
+            @Override
+            public ResponseModel prepareFailView(String error) {
+                fail("Use case failure is unexpected");
+                return null;
+            }
+        };
+
+        GameFactoryInterface gameFactory = new GameFactory();
+        CheckInputBoundary interactor = new CheckInteractor(presenter, gameFactory);
+
+        RequestModel input = new RequestModel(
+                0, 0, 0, new int[]{100, 100},
+                new String[]{"SA", "HA"}, new String[]{"SK", "HK"}, new String[]{"DA", "DK", "DQ", null, null},
+                new String[2], new String[2], new String[5], 0, new boolean[]{true, true},
+                new int[]{0, 0}, new String[]{"CA", "CK", "CQ", "CJ", "C10"}, "0", ""
+        );
+        interactor.create(input);
+    }
+
+    /**
      * Test case for a 2 player game when player 2 checks after player 1 checks
      */
     @Test
-    void player2Call() {
+    void player2Check() {
         CheckPresenter presenter = new CheckPresenter() {
             @Override
             public ResponseModel prepareSuccessView(ResponseModel outputData) {
