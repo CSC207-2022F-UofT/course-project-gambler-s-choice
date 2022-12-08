6899b75c8d13f5478d9a814836ceeae23e075848
eric <ericshi97@gmail.com>
2022-12-08 01:25:39 -0500

    Added scaffolding of FoldInteractorTest

---
+++ src/test/java/game_use_case/FoldInteractorTest.java
@@ -0,0 +1,27 @@
+package game_use_case;
+
+import game_entities.GameFactory;
+import game_entities.GameFactoryInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoldInteractorTest {
+
+    @Test
+    void secondPlayerFold() {
+        //check if the second player can fold - should output true
+    }
+
+    @Test
+    void allPlayerFold() {
+        //check what happens if all players fold
+    }
+
+    @Test
+    void firstPlayerFold() {
+        //check if the first player can fold - they should not be able to fold
+    }
+
+}
