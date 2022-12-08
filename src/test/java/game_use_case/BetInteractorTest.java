3f8f4a9eb166eda1c1040fb9f945905a5f337a25
Andy-Guo-13 <andy.guo21@ycdsbk12.ca>
2022-12-08 00:33:13 -0500

    Test case for bet use case

--- src/test/java/game_use_case/BetInteractorTest.java
+++ src/test/java/game_use_case/BetInteractorTest.java
@@ -17,10 +17,12 @@ public class BetInteractorTest {
         BetPresenter presenter = new BetPresenter() {
             @Override
             public ResponseModel prepareSuccessView(ResponseModel outputData) {
+                // Test for correct balance
                 assertEquals(75, outputData.getPlayerBalance()[0]);
                 assertEquals(100, outputData.getPlayerBalance()[1]);
                 assertEquals(25, outputData.getPlayerBets()[0]);
                 assertEquals(0, outputData.getPlayerBets()[1]);
+                // Test for correct user turn and last bet amount
                 assertEquals(1, outputData.getCurrentPlayer());
                 assertEquals(25, outputData.getCurrentBet());
                 return null;
@@ -38,9 +40,47 @@ public class BetInteractorTest {
 
         RequestModel input = new RequestModel(
                 0, 0, 0, new int[]{100, 100},
-                new String[]{"SA", "HA"}, new String[]{"SK", "HK"}, new String[]{"DA", "DK", "DQ", "DJ", "D10"},
+                new String[]{"SA", "HA"}, new String[]{"SK", "HK"}, new String[]{"DA", "DK", "DQ", null, null},
                 new String[2], new String[2], new String[5], 0, new boolean[]{true, true},
                 new int[]{0, 0}, new String[]{"CA", "CK", "CQ", "CJ", "C10"}, 25
+        );
+        interactor.create(input);
+    }
+
+    /**
+     * Test case for a 2 player game when player 2 bets 25 dollars
+     */
+    @Test
+    void player2betEndOfRound() {
+        BetPresenter presenter = new BetPresenter() {
+            @Override
+            public ResponseModel prepareSuccessView(ResponseModel outputData) {
+                // Test for correct balance
+                assertEquals(75, outputData.getPlayerBalance()[0]);
+                assertEquals(75, outputData.getPlayerBalance()[1]);
+                assertEquals(25, outputData.getPlayerBets()[0]);
+                assertEquals(25, outputData.getPlayerBets()[1]);
+                // Test for correct user turn and last bet amount
+                assertEquals(0, outputData.getCurrentPlayer());
+                assertEquals(25, outputData.getCurrentBet());
+                return null;
+            }
+
+            @Override
+            public ResponseModel prepareFailView(String error) {
+                fail("Use case failure is unexpected");
+                return null;
+            }
+        };
+
+        GameFactoryInterface gameFactory = new GameFactory();
+        BetInputBoundary interactor = new BetInteractor(presenter, gameFactory);
+
+        RequestModel input = new RequestModel(
+                1, 0, 0, new int[]{75, 100},
+                new String[]{"SA", "HA"}, new String[]{"SK", "HK"}, new String[]{"DA", "DK", "DQ", null, null},
+                new String[2], new String[2], new String[5], 0, new boolean[]{true, true},
+                new int[]{25, 0}, new String[]{"CA", "CK", "CQ", "CJ", "C10"}, 25
         );
         interactor.create(input);
     }
