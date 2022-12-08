93912167ed9355ba1c0098e40b626cf352833ee1
Jefferson Liu <35241738+SeIIout@users.noreply.github.com>
2022-12-07 22:01:29 -0500

    Added update methods so the accounts variables will update alongside the file

--- src/main/java/menu_use_case/MenuFileChecker.java
+++ src/main/java/menu_use_case/MenuFileChecker.java
@@ -3,14 +3,17 @@ package menu_use_case;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MenuFileChecker implements MenuDSGateway{
 
     private final ArrayList<String[]> accounts = new ArrayList<String[]>();
 
+    private final File usersFile;
+
     public MenuFileChecker(String txtPath) throws IOException{
-        File usersFile = new File(txtPath);
+        usersFile = new File(txtPath);
 
         if (!(usersFile.createNewFile())) {
             Scanner scanner = new Scanner(usersFile);
@@ -25,6 +28,12 @@ public class MenuFileChecker implements MenuDSGateway{
 
     @Override
     public boolean sufficientBalance(String user) {
+        try{
+            update();
+        }catch (Exception e){
+            return false;
+        }
+
         for (String[] account: accounts){
             if (account[0].equals(user) && Integer.parseInt(account[3]) >= 100){
                 return true;
@@ -35,6 +44,12 @@ public class MenuFileChecker implements MenuDSGateway{
 
     @Override
     public int getBalance(String user) {
+        try{
+            update();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         for (String[] account: accounts){
             if (account[0].equals(user)){
                 Integer.parseInt(account[3]);
@@ -42,4 +57,30 @@ public class MenuFileChecker implements MenuDSGateway{
         }
         return 0;
     }
+
+
+    /**
+     * Private helper method that updates the accounts instance variable
+     */
+    private void update() throws IOException{
+        Scanner scanner = new Scanner(usersFile);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] account = line.split(", ");
+            if (!isInAccounts(account)){
+                accounts.add(account);
+            }
+        }
+        scanner.close();
+    }
+
+    private boolean isInAccounts (String[] account){
+        for(String [] acc: accounts){
+            if (Arrays.equals(account, acc)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
