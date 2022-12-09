ed5be78afeffa2c5eeb7bdc467c951fcac79b49e
Andy-Guo-13 <andy.guo21@ycdsbk12.ca>
2022-12-08 22:29:38 -0500

    Add javadoc for menu use case

--- src/main/java/menu_use_case/MenuInputBoundary.java
+++ src/main/java/menu_use_case/MenuInputBoundary.java
@@ -1,6 +1,12 @@
 package menu_use_case;
-
+/**
+ * Used as an input boundary for clean architecture
+ */
 public interface MenuInputBoundary {
-
+    /**
+     * The function will create a new response model given the request model
+     * @param menuRequestModel the inputted request model
+     * @return a response model with updated values
+     */
     MenuResponseModel create(MenuRequestModel menuRequestModel);
 }
